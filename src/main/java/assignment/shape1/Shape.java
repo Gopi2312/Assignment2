bef3acd29bc41e57a309c0b36abbb7845b8b345e
unknown <gopinathan.gm@tringapps.com>
2023-02-27 23:21:51 +0530

    initial

---
+++ src/main/java/assignment/shape1/Shape.java
@@ -0,0 +1,64 @@
+package assignment.shape1;
+import java.util.logging.*;
+import java.util.*;
+class Shape
+{
+    float height;
+    float width;
+    float base;
+    float side1;
+    float side2;
+    float radius;
+    int x;
+    boolean b = true;
+    Scanner sc = new Scanner(System.in);
+    Logger logger = Logger.getLogger("hi");
+
+    Shape(int x,float radius) {
+    	this.x = x;
+        this.radius = radius;
+    }
+    Shape(int x,float height,float width) {
+    	this.x = x;
+        this.height = height;
+        this.width = width;
+    }
+    Shape(int x,float base,float side1,float side2) {
+    	this.x = x;
+        this.base = base;
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+
+    void perimeter() {
+        if (x == 1) {
+            float circle = (float) (2 * 3.14 * radius);
+            logger.log(Level.INFO,()->"Perimeter of circle:" + circle);
+        } else if (x == 2) {
+            float rectangle = 2 * (height + width);
+            logger.log(Level.INFO,()->"Perimeter of circle:" + rectangle);
+        } else if (x == 3) {
+            float triangle = base + side1 + side2;
+            logger.log(Level.INFO,()->"Perimeter of circle:" + triangle);
+        } else {
+            b = false;
+            System.exit(0);
+        }
+    }
+
+    void area() {
+        if (x == 1) {
+            float circle = (float) (3.14 * radius * radius);
+            logger.log(Level.INFO,()->"Perimeter of circle:" + circle);
+        } else if (x == 2) {
+            float rectangle = height * width;
+            logger.log(Level.INFO,()->"Perimeter of circle:" + rectangle);
+        } else if (x == 3) {
+            float triangle = (float) (1.0 / 2.0 * (base * side1));
+            logger.log(Level.INFO,()->"Perimeter of circle:" + triangle);
+        } else {
+            b = false;
+            System.exit(0);
+        }
+    }
+}
--- src/main/java/assignment/shape1/Shape1.java
+++ src/main/java/assignment/shape1/Shape1.java
@@ -1,76 +1,45 @@
 package assignment.shape1;
-
+import java.util.logging.*;
 import java.util.*;
-public class Shape1 
-{
-    float height;
-    float width;
-    float base;
-    float side1;
-    float side2;
-    float radius;
-    static int x;
-    boolean b = true;
-    Scanner sc = new Scanner(System.in);
-
-    Shape1() {
-        while (b) {
-            System.out.print("1.Circle\n2.Rectangle\n3.Triangle\n4.Exit\nEnter your option 1 or 2 or 3 or 4: ");
-            x = sc.nextInt();
-            if (x == 1) {
-                System.out.print("Enter Circle Radius:");
-                radius = sc.nextFloat();
-            } else if (x == 2) {
-                System.out.print("Enter Rectangle Height:");
-                height = sc.nextFloat();
-                System.out.print("Enter Rectangle Width:");
-                width = sc.nextFloat();
-            } else if (x == 3) {
-                System.out.print("Enter Triangle base: ");
-                base = sc.nextFloat();
-                System.out.print("Enter Triangle side1: ");
-                side1 = sc.nextFloat();
-                System.out.print("Enter Triangle Side2: ");
-                side2 = sc.nextFloat();
-            }
-            perimeter();
-            area();
-        }
-    }
-
-    void perimeter() {
-        if (x == 1) {
-            float cirPer = (float) (2 * 3.14 * radius);
-            System.out.println("Perimeter of circle:" + cirPer);
-        } else if (x == 2) {
-            float recPer = 2 * (height + width);
-            System.out.println("Perimeter of Rectangle:" + recPer);
-        } else if (x == 3) {
-            float triPer = base + side1 + side2;
-            System.out.println("Perimeter of Triangle:" + triPer);
-        } else {
-            b = false;
-            System.exit(0);
-        }
-    }
-
-    void area() {
-        if (x == 1) {
-            float cirPer = (float) (3.14 * radius * radius);
-            System.out.println("area of circle:" + cirPer);
-        } else if (x == 2) {
-            float recPer = height * width;
-            System.out.println("area of Rectangle:" + recPer);
-        } else if (x == 3) {
-            float triPer = 1 / 2 * (base * side1);
-            System.out.println("Perimeter of Triangle:" + triPer);
-        } else {
-            b = false;
-            System.exit(0);
-        }
-    }
+class Shape1{
     public static void main( String[] args )
     {
-    	Shape1 sp = new Shape1();
+        int x;
+        boolean b = true;
+        Scanner sc = new Scanner(System.in);
+        Logger logger = Logger.getLogger("hi");
+    	while (b) {
+    		Shape sp;
+            logger.info("1.Circle\n2.Rectangle\n3.Triangle\n4.Exit\nEnter your option 1 or 2 or 3 or 4: ");
+            x = sc.nextInt();
+                if (x == 1) {
+                    logger.info("Enter Circle Radius:");
+                    float radius = sc.nextFloat();
+                    sp = new Shape(x,radius);
+                    sp.perimeter();
+                    sp.area();
+                } else if (x == 2) {
+                    logger.info("Enter Rectangle Height:");
+                    float height = sc.nextFloat();
+                    logger.info("Enter Rectangle Width:");
+                    float width = sc.nextFloat();
+                    sp = new Shape(x,height,width);
+                    sp.perimeter();
+                    sp.area();
+                } else if (x == 3) {
+                    logger.info("Enter Triangle base: ");
+                    float base = sc.nextFloat();
+                    logger.info("Enter Triangle side1: ");
+                    float side1 = sc.nextFloat();
+                    logger.info("Enter Triangle Side2: ");
+                    float side2 = sc.nextFloat();
+                    sp = new Shape(x,base,side1,side2);
+                    sp.perimeter();
+                    sp.area();
+                }else {
+                    b = false;
+                    System.exit(0);
+                }
+            }
     }
 }
